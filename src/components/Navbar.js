974fd4fcf182483569a942d7acb20cbc5f1a9896
iamabhijeet2003 <abhijeet3016@gmail.com>
2024-08-25 16:27:24 +0200

    add new styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,64 @@
-import React from "react";
+import React, {useState} from "react";
 import { Navbar, Container, Nav, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { CgGitFork } from "react-icons/cg";
+import {
+    AiFillStar,
+    AiOutlineHome,
+    AiOutlineFundProjectionScreen,
+    AiOutlineUser,
+  } from "react-icons/ai";
+import logo from "../Assets/logo.png";
+
 function NavBar(){
+
+    const [expand, updateExpanded] = useState(false);
+    const [navColour, updateNavbar] = useState(false);
+
+    function scrollHandler(){
+        if(window.scrollY >= 20){
+            updateNavbar(true);
+        }else{
+            updateNavbar(false);
+        }
+    }
+
+    window.addEventListener("scroll", scrollHandler);
+
+
     return (
-        <Navbar>
+        <Navbar
+            expanded={expand}
+            fixed="top"
+            expand="md"
+            className={navColour ? "sticky" : "navbar"}
+        >
             <Container>
-                <Navbar.Collapse>
-                    <Nav>
+                <Navbar.Brand href="/" className="d-flex">
+                    <img src={logo} className="img-fluid logo" alt="brand"/>
+                </Navbar.Brand>
+                
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="ms-auto" defaultActiveKey="#home">
                        {/* Home */}
-                       <Nav.Item as={Link} to="/">
-                            Home
+                       <Nav.Item>
+                            <Nav.Link as={Link} to="/" onClick={() => updateExpanded(false)}>
+                            <AiOutlineHome style={{ marginBottom: "2px" }} /> Home
+                            </Nav.Link>
                        </Nav.Item>
                        {/* Home */}
 
 
                        {/* Github Repository */}
-                       <Nav.Item>
+                       <Nav.Item className="fork-btn">
                         <Button
                             href="https://github.com/iamabhijeet2003/iamabhijeet"
                             target="_blank"
+                            className="fork-btn-inner"
                         >
-                            Star this repo
+                            <CgGitFork style={{ fontSize: "1.2em"}} />
+                            {" "}
+                            <AiFillStar style={{ fontSize: "1.2em"}} />
                         </Button>
                        </Nav.Item> 
                        {/* Github Repository */}
