e75adbaf7caf242ae35cef36abe7eb49dbbd746d
iamabhijeet2003 <abhijeet3016@gmail.com>
2024-08-25 16:28:23 +0200

    add new links and styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,60 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import {
+    AiFillGithub,
+    AiOutlineTwitter,
+  } from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
 
 function Footer(){
     let date = new Date();
     let year = date.getFullYear();
     return(
-        <Container>
+        <Container fluid className="footer" >
             <Row>
-                <Col>
+                <Col md="4" className="footer-copywright">
                     Designed and Developed by Abhijeet Singh
                 </Col>
 
-                <Col>
+                <Col md="4" className="footer-copywright">
                 Copyright © {year} AS
                 </Col>
+
+                <Col md="4" className="footer-body">
+          <ul className="footer-icons">
+            <li className="social-icons">
+              <a
+                href="https://github.com/iamabhijeet2003"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <AiFillGithub />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://twitter.com/iamabhijeet2003"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <AiOutlineTwitter />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://www.linkedin.com/in/iamabhijeet2003/"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <FaLinkedinIn />
+              </a>
+            </li>
+          </ul>
+        </Col>
             </Row>
         </Container>
     )
