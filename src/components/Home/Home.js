d4ca993dcb24226314b38908d0e22fe538db7d4d
iamabhijeet2003 <abhijeet3016@gmail.com>
2024-08-28 23:53:12 +0200

    add image

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Container, Row, Col} from "react-bootstrap";
 import Particle from "../Particle";
 import Type from "./Type";
+import Home2 from "./Home2";
+import homeLogo from "../../Assets/home-main.svg"
 function Home(){
     return(
         <section>
@@ -26,9 +28,19 @@ function Home(){
                                 <Type />    
                             </div>
                         </Col>
+
+                        <Col>
+                            <img 
+                                src={homeLogo}
+                                alt="programmer on desk"
+                                className="img-fluid"
+                                style={{maxHeight:"450px"}}
+                            />
+                        </Col>
                     </Row>
                 </Container>
             </Container>
+            <Home2 />
         </section>
     )
 }
