e75ce405f2bf26f1d58b9f8d00315e10d2954f71
iamabhijeet2003 <abhijeet3016@gmail.com>
2024-09-08 22:14:11 +0200

    add new component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Particle from "../Particle";
 import Techstack from "./TechStack";
+import Toolstack from "./ToolStack";
 function About(){
     return(
         <Container fluid className="about-section">
@@ -24,6 +25,11 @@ function About(){
                 </h1>
 
             <Techstack />
+
+            <h1 className="project-heading">
+                <strong className="purple">Tools</strong> I use
+            </h1>
+            <Toolstack />
         </Container>
     )
 }
